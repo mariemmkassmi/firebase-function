3e84509ce79e514768a5eb9650206fc7d62c2661
mariemmkassmi <mariem.mkassmi@bearstudio.fr>
2023-08-18 15:57:43 +0100

    ajout le deal selon le nom de pipeline

--- functions/index.js
+++ functions/index.js
@@ -1,18 +1,19 @@
 const functions = require('firebase-functions');
 const { logger } = require("firebase-functions");
 const axios = require('axios');
-const slackApiKey = "xoxb-5683988392193-5710152422336-p4rAfRbaxxrsTkYMNlb3Ayjk";
+const slackApiKey = "xoxb-5683988392193-5758966778261-xvPI8rTow0fl76r1QVxYvX9V";
 
 exports.send = functions.https.onRequest(async (request, response) => {
     try {
-        const { title, owner_name, person_name, formatted_value } = request.body.current;
+        const { title, owner_name, person_name, formatted_value,pipeline_id } = request.body.current;
         const { channel,message} = request.body;
+          const pipelineResponse = await axios.get(`https://api.pipedrive.com/v1/pipelines/${pipeline_id}`, {
+            params: {
+                api_token: '07865ff46dea373140f8eb6da111be066e87cf59' 
+            }
+        });
         const axiosResponse = await axios.post('https://slack.com/api/chat.postMessage', {
-            channel: title && title.includes("person")
-            ? "random"
-            : title && title.includes("onbording")
-                ? "100"
-                : "money",
+            channel: pipelineResponse.data.data.name.includes("swi")? "money" :"random",
             text:
             `
             Title: ${title}
@@ -26,13 +27,14 @@ exports.send = functions.https.onRequest(async (request, response) => {
                 'Authorization': 'Bearer ' + slackApiKey,
             }
         });
+
         if (!axiosResponse.data.ok) {
             throw new Error(`Slack API Error: ${axiosResponse.data.error}`);
         }
+
         response.status(200).send({ success: true, message: 'Message sent to Slack successfully' });
     } catch (error) {
         console.error('Slack Error:', error.message);
         response.status(500).send({ success: false, message: 'Error sending message to Slack' });
     }
 });
-
